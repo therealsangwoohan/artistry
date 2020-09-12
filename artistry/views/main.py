d4cc6ecd258726950d1066c3aa16623ec136d268
cyberpunkninja76 <cyberpunkninja76@gmail.com>
2020-09-12 15:46:10 -0400

    Update

--- artistry/views/main.py
+++ artistry/views/main.py
@@ -1,10 +1,27 @@
 from artistry import app
-from flask import render_template, redirect, request, send_file
+from flask import render_template, redirect, request
 from artistry.core.lyrics import find_lyrics
 import networkx as nx
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
+
+
+def getImg(artistName):
+    G = nx.Graph()
+    G.add_edge(artistName, "Sang3o")
+    G.add_edge(artistName, "50 Cent")
+    G.add_edge(artistName, "2 Pac")
+    nx.draw(G, with_labels=True)
+    img = BytesIO()
+    plt.savefig(img)
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(img.getvalue()).decode('utf8')
+    img.seek(0)
+    plt.clf()
+    return pngImageB64String
 
 
 @app.route("/")
@@ -18,17 +35,7 @@ def searchArtist():
         # artistLyrics = find_lyrics(request.form["artistName"])
         # return render_template("artist_info.html",
         #                        artistLyrics=artistLyrics)
-        G = nx.Graph()
-        G.add_edge("Eminem", "Sang3o")
-        G.add_edge("Eminem", "50 Cent")
-        G.add_edge("Eminem", "2 Pac")
-        nx.draw(G, with_labels=True)
-        img = BytesIO()
-        plt.savefig(img)
-        pngImageB64String = "data:image/png;base64,"
-        pngImageB64String += base64.b64encode(img.getvalue()).decode('utf8')
-        img.seek(0)
-        plt.clf()
-        return render_template("artist_info.html", img=pngImageB64String)
+        return render_template("artist_info.html",
+                               img=getImg(request.form["artistName"]))
 
     return render_template("search_artist.html")
